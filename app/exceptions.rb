2d92e00462a9692db476d8df7f96e2ad02854ffc
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-01-18 23:09:41 +0100

    Remove an unused file with exceptions.

--- app/exceptions.rb
+++
@@ -1,8 +0,0 @@
-class PredictionAnswerDoNotMatch < StandardError
-  attr_reader :component, :answer
-
-  def initialize(component, answer)
-    @component = component
-    @answer    = answer
-  end
-end
