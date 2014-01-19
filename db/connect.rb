0032879dd5b255a364c7b8de06f90b1ce43b196b
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-01-19 18:42:17 +0100

    Use a password for the database

--- db/connect.rb
+++ db/connect.rb
@@ -6,6 +6,9 @@ host = database['host']
 port = database['port']
 name = database['name']
 user = database['user']
+pass = database['pass']
+
 url = "postgres://#{host}:#{port}/#{name}?user=#{user}"
+url += "&password=#{pass}" if pass
 
 DB = Sequel.connect(url)
