b3aef82e8428db34c7f6a81499789a14b9585feb
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-01-25 11:18:21 +0100

    Expose user names.

--- app/entities/player.rb
+++ app/entities/player.rb
@@ -6,6 +6,8 @@ module Entities
     expose :token_expiration, if: :token, format_with: :timestamp
 
     expose :nickname
+    expose :first_name
+    expose :last_name
 
     expose :social_associations, using: SocialAssociation, as: :social
   end
