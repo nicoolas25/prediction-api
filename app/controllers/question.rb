aef546c521dcae1d3b696da86180dedc8a14b844
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-02-27 10:22:47 +0100

    Fix the order for outdated questions instead of the answered one

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -21,14 +21,14 @@ module Controllers
 
         desc "List the answered questions of a player"
         get 'global/answered' do
-          questions = Domain::Question.global.open.answered_by(player).with_locale(@locale).ordered(:desc).all
+          questions = Domain::Question.global.open.answered_by(player).with_locale(@locale).ordered.all
           friend_service = Domain::Services::Friend.new(player, questions.map(&:id))
           present questions, with: Entities::Question, locale: @locale, friend_service: friend_service
         end
 
         desc "List the answered questions of a player"
         get 'global/outdated' do
-          questions = Domain::Question.global.expired.answered_by(player).with_locale(@locale).ordered.all
+          questions = Domain::Question.global.expired.answered_by(player).with_locale(@locale).ordered(:desc).all
           friend_service = Domain::Services::Friend.new(player, questions.map(&:id))
           present questions, with: Entities::Question, locale: @locale, player: player, friend_service: friend_service
         end
