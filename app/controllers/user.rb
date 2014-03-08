6de83e50a687aed727ad5773b3546ac07db32854
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-03-08 21:03:52 +0100

    Fix user controller

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -10,23 +10,22 @@ module Controllers
     namespace :users do
       namespace ':uid' do
         params { requires :uid, type: String }
-        before { @user = params[:uid] == 'me' ? player : Domain::Player.first!(id: params[:uid]) }
+        before {
+          @user = params[:uid] == 'me' ? player : Domain::Player.first!(id: params[:uid])
+          @mine = @user == player
+        }
 
         desc "Show the details of a player (account page too)"
         get do
-          present @user, with: Entities::Friend, details: true
+          present @user, with: Entities::Friend, details: true, mine: @mine
         end
 
         desc "List the open questions for a player"
         get 'friends' do
-          friends =
-            if user == player
-              @user.friends_dataset.eager(:social_associations).all
-            else
-              @user.friends
-            end
-
-          present friends, with: Entities::Friend, mine: user == player
+          friends = @mine ?
+            @user.friends_dataset.eager(:social_associations).all :
+            @user.friends
+          present friends, with: Entities::Friend, mine: @mine
         end
       end
     end
