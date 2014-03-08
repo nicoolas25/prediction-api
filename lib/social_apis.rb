01cfa4805d724fc2fc8217d8b7bed8b653b9ce24
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-03-08 15:19:44 +0100

    Remove twitter provider

--- lib/social_apis.rb
+++ lib/social_apis.rb
@@ -4,17 +4,15 @@ module SocialAPI
   autoload :Base,       './lib/social_apis/base'
   autoload :Facebook,   './lib/social_apis/facebook'
   autoload :GooglePlus, './lib/social_apis/google_plus'
-  autoload :Twitter,    './lib/social_apis/twitter'
 
   # Do not reorder this!
-  PROVIDERS = %w(facebook googleplus twitter).freeze
+  PROVIDERS = %w(facebook googleplus).freeze
   SYMETRIC_FRIENDSHIP_PROVIDERS = [0].freeze
 
   def self.for(provider, token, id=nil)
     case provider
     when 'facebook', 0   then Facebook.new(provider, token, id)
     when 'googleplus', 1 then GooglePlus.new(provider, token, id)
-    when 'twitter', 2    then Twitter.new(provider, token, id)
     else nil
     end
   end
