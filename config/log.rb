058697388c9758d0c84b932faa12d098646dc0fe
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-03-22 19:28:04 +0100

    Remove log for html / js bodies.

--- config/log.rb
+++ config/log.rb
@@ -7,8 +7,9 @@ env = ENV['RACK_ENV'] || 'app'
 LOGGER ||= Logger.new("./log/#{env}.log", 5, 100.megabytes)
 
 class LoggerMiddleware
-  FORMAT_BGN = %{Receive %s "%s%s" from %s}
-  FORMAT_END = %{Completed %d (%0.6fs)\nBody: %s\n}
+  FORMAT_BGN         = %{Receive %s "%s%s" from %s}
+  FORMAT_END         = %{Completed %d (%0.6fs)\nBody: %s\n}
+  FORMAT_END_WO_BODY = %{Completed %d (%0.6fs)\n}
 
   def initialize(app)
     @app = app
@@ -18,7 +19,7 @@ class LoggerMiddleware
     began_at = Time.now
     log_bgn(env)
     status, header, body = @app.call(env)
-    log_end(status, began_at, body)
+    log_end(status, header, began_at, body)
     [status, header, body]
   end
 
@@ -34,12 +35,16 @@ class LoggerMiddleware
     LOGGER.info(msg)
   end
 
-  def log_end(status, began_at, body)
-    body = body.instance_eval{ @body } if body.kind_of?(Rack::BodyProxy)
-    body = body.body                   if body.kind_of?(Rack::Response)
-    body = body.first                  if body.kind_of?(Array)
+  def log_end(status, header, began_at, body)
+    if header['Content-Type'] =~ /json/
+      body = body.instance_eval{ @body } if body.kind_of?(Rack::BodyProxy)
+      body = body.body                   if body.kind_of?(Rack::Response)
+      body = body.first                  if body.kind_of?(Array)
+      msg  = FORMAT_END % [ status.to_s[0..3], Time.now - began_at, body ]
+    else
+      msg = FORMAT_END_WO_BODY % [ status.to_s[0..3], Time.now - began_at]
+    end
 
-    msg = FORMAT_END % [ status.to_s[0..3], Time.now - began_at, body ]
     LOGGER.info(msg)
   end
 end
