79d3a2d0ccdb80c96cf776e48451bc602c236f1c
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-03-29 23:32:57 +0100

    Fixing whenever / chruby

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ set :environment_variable, 'RACK_ENV'
 set :ruby_v, '2.1.0'
 
 job_type :rake,
-  "source /etc/profile.d/chruby && chruby :ruby_v && cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
+  "source /etc/profile.d/chruby.sh && chruby :ruby_v && cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
 
 every 10.minutes do
   rake "test:whenever"
