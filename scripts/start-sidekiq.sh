0e0202924a7b904e3a4663de7f8724e39b9b5350
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-03-29 19:32:02 +0100

    Fix a typo

--- scripts/start-sidekiq.sh
+++ scripts/start-sidekiq.sh
@@ -10,4 +10,4 @@ pid_file=/var/www/api/shared/pids/sidekiq.pid
 log_file=/var/www/api/shared/log/sidekiq.log
 
 cd "/var/www/api/current"
-nohup bundle exec sidekiq -e production -C $config_file -P $pidfile -r ./app/workers.rb >> $log_file 2>&1 &
+nohup bundle exec sidekiq -e production -C $config_file -P $pid_file -r ./app/workers.rb >> $log_file 2>&1 &
