45b0188b5f5ad9cf44fa1b72a08043a8866d8567
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-05-23 23:38:59 +0200

    Fixing players until mexico...

--- scripts/tools/fetch_players/fetch_image.rb
+++ scripts/tools/fetch_players/fetch_image.rb
@@ -1,14 +1,50 @@
+require 'i18n'
 require 'nokogiri'
+require 'htmlentities'
+require 'yaml'
+
+PLAYERS  = YAML.load_file('./resources/players.yml')
+FILENAME = "playerdata.xml"
+FILEPATH = File.join(File.dirname(__FILE__), FILENAME)
+
+DECODER = HTMLEntities.new
 
 def fetch_file
-	system "wget", "http://c3420952.r52.cf0.rackcdn.com/playerdata.xml"
+  system "wget", "http://c3420952.r52.cf0.rackcdn.com/#{FILENAME}"
+  system "mv", FILENAME, FILEPATH
 end
 
-def imageFor firstname, lastname
-	doc = Nokogiri::XML(File.open("playerdata.xml"))
-	res = doc.xpath("//P").select { |node| node["f"]==firstname.downcase.capitalize and node["s"] == lastname.upcase}
-	puts "http://cdn.soccerwiki.org/images/player/" + res[0]["i"]
+def players_hash
+  @players_hash ||= begin
+    doc = Nokogiri::XML(File.open(FILEPATH))
+    hash = {}
+    doc.xpath("//P").select do |node|
+      key = "#{node['f']} #{node['s']}"
+      key = DECODER.decode(key)
+      key = I18n.transliterate(key)
+      key = key.downcase
+      hash[key] ||= []
+      hash[key]  << node['i'] if node['i']
+    end
+    hash
+  end
 end
 
+def imageFor(name)
+  name = I18n.transliterate(name.downcase)
+  if files = players_hash[name]
+    "http://cdn.soccerwiki.org/images/player/#{files[0]}"
+  else
+    'missing'
+  end
+end
 
-imageFor "karim", "benzema"
+PLAYERS.each do |country, players|
+  players.each do |player|
+    if player['url'] == 'missing'
+      player['url'] = imageFor(player['name'])
+    end
+  end
+end
+
+puts PLAYERS.to_yaml
