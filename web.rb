57a4cc0bac0a93489970133e89fab5f7f19ea988
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-05-25 22:48:45 +0200

    Add statistics.

--- web.rb
+++ web.rb
@@ -21,6 +21,14 @@ module Prediction
 
     get '/' do
       @t = WEB_CONFIG[locale]['home']
+      @statistics = {
+        'players'        => Domain::Player.dataset.count,
+        'won_cristals'   => Domain::Participation.dataset.select(Sequel.function(:sum, :winnings).as(:winnings)).first.values[:winnings],
+        'participations' => Domain::Participation.dataset.count,
+        'past_questions' => Domain::Question.dataset.expired.count,
+        'open_questions' => Domain::Question.dataset.open.count,
+        'next_questions' => Domain::Question.dataset.where(Sequel.expr(:reveals_at) > Time.now).count
+      }
       slim :home
     end
 
