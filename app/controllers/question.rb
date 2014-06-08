b3317c73cb70100d86db7b3469ff414dc1e97810
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-06-08 22:59:38 +0200

    Add the question tagged for fiends answered questions.

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -85,6 +85,25 @@ module Controllers
             made_prediction: false
         end
 
+        desc "List the open questions for a player where a friend participate"
+        params do
+          requires :locale, type: String, regexp: /^(fr)|(en)|(pt)|(es)$/
+          requires :tag_id, type: Integer
+        end
+        get 'friends/open/tags/:tag_id' do
+          if tag = Domain::Tag.first(id: params[:tag_id])
+            questions = Domain::Question.global.open.for(player).answered_by_friends(player).with_locale(@locale).ordered.tagged_with(tag).with_tags.all
+            friend_service = Domain::Services::FriendQuestion.new(player, questions.map(&:id))
+            present questions,
+              with: Entities::Question,
+              locale: @locale,
+              friend_service: friend_service,
+              made_prediction: false
+          else
+            fail!(:tag_not_found , 404)
+          end
+        end
+
         desc "Show the details of a question"
         params do
           requires :locale, type: String, regexp: /^(fr)|(en)|(pt)|(es)$/
